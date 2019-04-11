eb28721ae66b665681af7286e4d4292b09b9f188
Muniir Gopaul <mgopaul1@amaris.com>
2019-04-11 16:02:29 +0400

    fontawesome

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,4 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import '@fortawesome/fontawesome-free/css/all.css'
+
 
 // ------------------------------------------------------------------
 
