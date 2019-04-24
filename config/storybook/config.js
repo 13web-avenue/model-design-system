153057d6bd58a28e7a7629ea921700cd707a0cde
Muniir Gopaul <mgopaul1@amaris.com>
2019-04-24 10:40:04 +0400

    Reviews

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -67,38 +67,69 @@ import '../../assets/stylesheets/cssGlobalOverrides.scss'
 addParameters({
   options: {
     /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
      * display panel that shows addon configurations
      * @type {Boolean}
      */
     showPanel: true,
     /**
-     * display floating search box to search through stories
-     * @type {Boolean}
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
      */
-    showSearchBox: false,
-    /**
-     * show addon panel as a vertical panel on the right
-     * @type {Boolean}
-     */
-    panelPosition : 'bottom',
-    /**
-     * show/hide tool bar
-     * @type {Boolean}
-     */
+    panelPosition: 'bottom',
     /**
      * sorts stories
      * @type {Boolean}
      */
-    sortStoriesByKind: true,    
-    isToolshown: true, // true by default
+    sortStoriesByKind: false,
     /**
-     * default themes
-     * Custom themes will be applied form UnifyThemes
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
      */
-    theme: UnifyTheme,
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: false,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    /**
+     * theme storybook, see link below
+     */
+    theme: undefined,
   },
-})
-
+});
 
 const req = require.context('../../src/', true, /.js$/)
 
