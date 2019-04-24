dbe72353d9591a97d2c06ab41a8ccab963fb7279
Muniir Gopaul <mgopaul1@amaris.com>
2019-04-24 12:43:13 +0400

    config maintainance

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -90,7 +90,7 @@ addParameters({
      * sorts stories
      * @type {Boolean}
      */
-    sortStoriesByKind: false,
+    sortStoriesByKind: true,
     /**
      * regex for finding the hierarchy separator
      * @example:
